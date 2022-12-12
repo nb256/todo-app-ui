45587b6f12bf021895f5e5f059868e8299d51f8a
Naim Buru <nbur4u@gmail.com>
2022-12-12 06:11:06 +0300

    feat: add removing functionality to the Todo component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,31 @@
 import { Checkbox, FormControlLabel } from "@mui/material";
-import styled from "@emotion/styled";
+import styled from "@emotion/styled/macro";
 import { useState } from "react";
 
 import useMarkTodoCompleted from "../hooks/useMarkTodoCompleted";
 import useMarkTodoUncompleted from "../hooks/useMarkTodoUncompleted";
+import removeIcon from "../assets/remove.svg";
+import useDeleteTodo from "../hooks/useDeleteTodo";
+
+const RemoveButton = styled.img`
+  width: 32px;
+  height: 32px;
+  padding: 10px;
+  cursor: pointer;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
 
 const Container = styled.div`
-  display: block;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  &:hover {
+    ${RemoveButton} {
+      display: block;
+    }
+  }
 `;
 
 export default function Todo({
@@ -19,6 +38,7 @@ export default function Todo({
   status: string;
 }) {
   const [checked, setChecked] = useState(status === "COMPLETED");
+  const [deleted, setDeleted] = useState(false);
 
   const markTodoCompleted = useMarkTodoCompleted();
   const markTodoUncompleted = useMarkTodoUncompleted();
@@ -33,12 +53,26 @@ export default function Todo({
     }
   };
 
+  const deleteTodo = useDeleteTodo();
+
+  const handleDelete = () => {
+    if (window.confirm("Are you sure you want to delete this todo?")) {
+      deleteTodo(id);
+      setDeleted(true);
+    }
+  };
+
+  if (deleted) {
+    return null;
+  }
+
   return (
     <Container>
       <FormControlLabel
         control={<Checkbox checked={checked} onChange={handleChange} />}
         label={title}
       />
+      <RemoveButton src={removeIcon} onClick={handleDelete} />
     </Container>
   );
 }
